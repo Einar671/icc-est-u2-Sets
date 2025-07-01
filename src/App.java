505432e3286eadbbebb5cf196158b820fb94605e
Einar671 <einarkaalhus2809@gmail.com>
2025-07-01 08:46:07 -0500

    Finalizacion de la practica

---
+++ src/App.java
@@ -0,0 +1,49 @@
+import java.util.Set;
+
+import controller.Sets;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Sets set = new Sets();
+        System.out.println("EINAR KAALHUS");
+        runHashSet(set);
+        runLinkedHashSet(set);
+        runTreeSet(set);
+        runTreeSetComparator(set);
+        runTreeSetComparatorRevez(set);
+    }
+
+    public static void runHashSet(Sets sets){
+        Set<String> elementos = sets.construirHashSet();
+        System.out.println("---Hash Set---");
+        System.out.println("Elementos del HashSet");
+        for(String s : elementos) System.out.println(s);
+    }
+
+    public static void runLinkedHashSet(Sets sets){
+        Set<String> elementos = sets.construirLinkedHashSet();
+        System.out.println("---Linked Hash Set---");
+        System.out.println("Elementos del LinkedHashSet"); 
+        for(String s : elementos) System.out.println(s);      
+    }
+
+    public static void runTreeSet(Sets sets){
+        Set<String> elementos = sets.construirTreeSet();
+        System.out.println("---Tree Set---");
+        System.out.println("Elementos del TreeSet (orden xxxxx)"); 
+        for(String s : elementos) System.out.println(s);      
+    }
+
+    public static void runTreeSetComparator(Sets sets){
+        Set<String> elementos = sets.construirTreeSetConComparador();
+        System.out.println("---Tree Set comparador---");
+        System.out.println("Elementos del TreeSet (orden xxxxx)"); 
+        for(String s : elementos) System.out.println(s);         
+    }
+    public static void runTreeSetComparatorRevez(Sets sets){
+        Set<String> elementos = sets.construirTreeSetConComparadorReves();
+        System.out.println("---Tree Set comparador al revez---");
+        System.out.println("Elementos del TreeSet (orden xxxxx)"); 
+        for(String s : elementos) System.out.println(s);         
+    }
+}
---
+++ src/controller/Sets.java
@@ -0,0 +1,97 @@
+package controller;
+
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets{
+
+    public Sets(){
+
+    }
+
+    public Set<String> construirHashSet(){
+        Set<String> palabras = new HashSet<>();
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop");
+        palabras.add("Pera");
+        palabras.add("Laptop");
+        return palabras;
+    }
+
+    public Set<String> construirLinkedHashSet(){
+        Set<String> palabras = new LinkedHashSet<>();
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop");
+        palabras.add("Pera");
+        palabras.add("Laptop");
+        return palabras;
+    }
+
+    public Set<String> construirTreeSet(){
+        Set<String> palabras = new TreeSet<>();
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop");
+        palabras.add("Pera");
+        palabras.add("Laptop");
+        return palabras;
+    }
+
+    public Set<String> construirTreeSetConComparador(){
+        Comparator<String> comparadorLongitud = new Comparator<String>() {
+            @Override
+            public int compare(String s1,String s2){
+                int result = Integer.compare(s1.length(), s2.length());
+                if(result==0){
+                    return s1.compareToIgnoreCase(s2);
+                }
+                return result;
+            }
+        };
+        Set<String> palabras = new TreeSet<>(comparadorLongitud);
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop");
+        palabras.add("Pera");
+        palabras.add("Laptop");
+        palabras.add("Celulas");
+
+        return palabras;
+    }
+        public Set<String> construirTreeSetConComparadorReves(){
+        Comparator<String> comparadorLongitud = new Comparator<String>() {
+            @Override
+            public int compare(String s1,String s2){
+                int result = Integer.compare(s2.length(), s1.length());
+                if(result==0){
+                    return s2.compareToIgnoreCase(s1);
+                }
+                return result;
+            }
+        };
+        Set<String> palabras = new TreeSet<>(comparadorLongitud);
+        palabras.add("Laptop");
+        palabras.add("Manzana");
+        palabras.add("Pera");
+        palabras.add("Celular");
+        palabras.add("Laptop");
+        palabras.add("Pera");
+        palabras.add("Laptop");
+        palabras.add("Celulas");
+
+        return palabras;
+    }
+}
